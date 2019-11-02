4b5253b95d6e8884c2822f04d9672a3a55c9d055
XiongAmao <onycat@live.cn>
2019-11-02 19:21:50 +0800

    update bind implement

--- bind/index.js
+++ bind/index.js
@@ -51,11 +51,12 @@ const bindES5 = function(thisArg) {
 
     if (this instanceof bound) {
       // 当传入的不是箭头函数
+      // bound的prototype 替换为 原函数的prototype
       if (fn.prototype) {
-        function F () {}
+        function F() {}
         F.prototype = fn.prototype
-        // bound的prototype 替换为 原函数的prototype
         bound.prototype = new F()
+        F = null
       }
 
       // 获取原函数执行的结果，这里的this等同于 new Fn() 时创建的实例
@@ -68,8 +69,8 @@ const bindES5 = function(thisArg) {
       if (isFunction || isObject) {
         return result
       }
-      
-      // 否则返回this 
+
+      // 否则返回this
       // fn.apply时，已经将this传递给 fn，模拟new的操作
       return this
     }
@@ -79,7 +80,57 @@ const bindES5 = function(thisArg) {
   return bound
 }
 
+// https://github.com/Raynos/function-bind
+const mdnImplementBind = () => {
+  var ERROR_MESSAGE = 'Function.prototype.bind called on incompatible '
+  var slice = Array.prototype.slice
+  var toStr = Object.prototype.toString
+  var funcType = '[object Function]'
+
+  return function bind(that) {
+    var target = this
+    if (typeof target !== 'function' || toStr.call(target) !== funcType) {
+      throw new TypeError(ERROR_MESSAGE + target)
+    }
+    var args = slice.call(arguments, 1)
+
+    var bound
+    var binder = function() {
+      if (this instanceof bound) {
+        var result = target.apply(this, args.concat(slice.call(arguments)))
+        if (Object(result) === result) {
+          return result
+        }
+        return this
+      } else {
+        return target.apply(that, args.concat(slice.call(arguments)))
+      }
+    }
+
+    var boundLength = Math.max(0, target.length - args.length)
+    var boundArgs = []
+    for (var i = 0; i < boundLength; i++) {
+      boundArgs.push('$' + i)
+    }
+    
+    // rewrite arguments
+    bound = Function(
+      'binder',
+      'return function (' + boundArgs.join(',') + '){ return binder.apply(this,arguments); }'
+    )(binder)
+
+    if (target.prototype) {
+      var Empty = function Empty() {}
+      Empty.prototype = target.prototype
+      bound.prototype = new Empty()
+      Empty.prototype = null
+    }
+
+    return bound
+  }
+}
 module.exports = {
   bind,
-  bindES5
+  bindES5,
+  mdnBind: mdnImplementBind()
 }
