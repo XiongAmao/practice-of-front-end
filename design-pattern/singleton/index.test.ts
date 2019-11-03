d6b81f6f2bafde314b1b99b1feacead113b99143
XiongAmao <onycat@live.cn>
2019-11-03 22:03:59 +0800

    feat: revise test api

--- design-pattern/singleton/index.test.ts
+++ design-pattern/singleton/index.test.ts
@@ -5,7 +5,7 @@ test('Instances should be the same Object.', () => {
   const sg2 = new Singleton()
   const ins = Singleton.instance
 
-  expect(sg).toEqual(sg2)
-  expect(sg2).toEqual(ins)
-  expect(sg).toEqual(ins)
+  expect(sg).toBe(sg2)
+  expect(sg2).toBe(ins)
+  expect(sg).toBe(ins)
 })
